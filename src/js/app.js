318bdf5d5d9daacc924c79a9b98285098550a300
Ahamd <aa224fn@student.lnu.se>
2019-10-01 03:02:54 +0200

    fixing post

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,47 @@
-// console.log('TCL: question1', question1.json())
-// const htmlBody = document.querySelector('body')
-// // console.log('TCL: htmlBody', htmlBody)
-// const htmlQuestion = document.createElement('p')
-// console.log('TCL: htmlQuestion', htmlQuestion)
-// const obj
 
-async function getQuestion () {
-  const res = await window.fetch('http://vhost3.lnu.se:20080/question/1', { mode: 'cors' })
+function form (data) {
+  const obj = data
+  const htmlBody = document.querySelector('body')
+  const form = document.createElement('form')
+  form.setAttribute('method', 'post')
+  const span = document.createElement('span')
+  span.textContent = obj.question
+  const br = document.createElement('br')
+  const br1 = document.createElement('br')
+  const button = document.createElement('button')
+  button.textContent = 'Next Question'
+  const input = document.createElement('input')
+  input.setAttribute('type', 'text')
+  form.appendChild(span)
+  form.appendChild(br)
+  form.appendChild(input)
+  form.appendChild(br1)
+  form.appendChild(button)
+  htmlBody.appendChild(form)
+  const answer = input.value
+  // postData(obj.nextURL, answer.json())
+  console.log('TCL: form -> answer.json()', answer.json())
+  button.addEventListener('click', () => {
+    const answer = input.value
+    postData(obj.nextURL, answer.json())
+    console.log('TCL: form -> answer.json()', answer.json())
+  })
+}
+
+async function getQuestion (id) {
+  await window.fetch(`http://vhost3.lnu.se:20080/question/${id}`, { mode: 'cors' })
     .then(
-      function (response) {
-        if (response.status !== 200) {
+      function (res) {
+        if (res.status !== 200) {
           console.log('Looks like there was a problem. Status Code: ' +
-            response.status)
+          res.status)
           return
         }
         // Examine the text in the response
-        response.json().then(function (data) {
+        res.json().then(function (data) {
           console.log(data)
           form(data)
-        //  obj = data
-        // htmlQuestion.textContent = obj.question
-        // htmlBody.appendChild(htmlQuestion)
+          return data
         })
       }
     )
@@ -28,46 +49,23 @@ async function getQuestion () {
       console.log('Fetch Error :-S', err)
     })
 }
-function form (data) {
-  const obj = data
-  const htmlBody = document.querySelector('body')
-  const form = document.createElement('form')
-  const span = document.createElement('span')
-  const br = document.createElement('br')
-  const button = document.createElement('button')
-  button.textContent = 'Next Question'
-  button.addEventListener('click', event => {
-            console.log(event)
-  })
-  span.textContent = obj.question
-  form.setAttribute('method', 'post')
-  const input = document.createElement('input')
-  input.setAttribute('type', 'text')
-  form.appendChild(span)
-  form.appendChild(br)
-  form.appendChild(input)
-  const answer = input.value
-  htmlBody.appendChild(form)
-}
-async function SendAnswer (input) {
-  const data = {
-    answer: input
-  }
-  const res = await window.fetch('http://vhost3.lnu.se:20080/answer/1', {
-    method: 'post',
+
+async function postData (url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
     headers: {
-      'Content-type': 'application/json'
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(data)
+    redirect: 'follow', // manual, *follow, error
+    referrer: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
   })
-    .then(json)
-    .then(function (data) {
-      console.log('Request succeeded with JSON response', data)
-    })
-    .catch(function (error) {
-      console.log('Request failed', error)
-    })
+  return response.json() // parses JSON response into native JavaScript objects
 }
 
-getQuestion()
-SendAnswer()
+getQuestion(1)
+// postData()
