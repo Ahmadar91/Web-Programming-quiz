581be7b88b3d1bda89f5ff29074ba01e532a789a
Ahmad AR <38329562+Ahmadar91@users.noreply.github.com>
2019-10-04 16:13:48 +0200

    fixing post

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,48 @@
+// import { spawn } from 'child_process'
+
 // import fetch from 'node-fetch'
+// function form (data) {
+//   const obj = data
+//   const htmlBody = document.querySelector('body')
+//   const form = document.createElement('form')
+//   form.setAttribute('method', 'post')
+//   //  form.setAttribute('action', obj.nextURL)
+//   const span = document.createElement('span')
+//   span.textContent = obj.question
+//   const br = document.createElement('br')
+//   const br1 = document.createElement('br')
+//   const button = document.createElement('button')
+//   button.setAttribute('type', 'submit')
+//   button.textContent = 'Next Question'
+//   const input = document.createElement('input')
+//   input.setAttribute('type', 'text')
+//   form.appendChild(span)
+//   form.appendChild(br)
+//   form.appendChild(input)
+//   form.appendChild(br1)
+//   form.appendChild(button)
+//   htmlBody.appendChild(form)
+//   const value = document.createElement('p')
+//   value.setAttribute('id', 'values')
+//   htmlBody.appendChild(value)
 
-function form (data) {
-  const obj = data
-  const htmlBody = document.querySelector('body')
-  const form = document.createElement('form')
-  form.setAttribute('method', 'post')
-  form.setAttribute('action', obj.nextURL)
-  const span = document.createElement('span')
-  span.textContent = obj.question
-  const br = document.createElement('br')
-  const br1 = document.createElement('br')
-  const button = document.createElement('button')
-  button.setAttribute('type', 'submit')
-  button.textContent = 'Next Question'
-  const input = document.createElement('input')
-  input.setAttribute('type', 'text')
-  form.appendChild(span)
-  form.appendChild(br)
-  form.appendChild(input)
-  form.appendChild(br1)
-  form.appendChild(button)
-  htmlBody.appendChild(form)
-  const value = document.createElement('p')
-  value.setAttribute('id', 'values')
-  htmlBody.appendChild(value)
-
-  const result = {
-    answer: ''
-  }
-  console.log(result)
-  const log = document.getElementById('values')
-  input.addEventListener('input', updateValue)
-  console.log(obj.nextURL)
-  function updateValue (e) {
-    log.textContent = e.target.value
-    result.answer = e.target.value
-  }
-  button.addEventListener('click', () => {
-    postData(obj.nextURL, result)
-  })
-}
+//   const result = {
+//     answer: answer
+//   }
+//   console.log(result)
+//   const log = document.getElementById('values')
+//   input.addEventListener('change', updateValue)
+//   console.log(obj.nextURL)
+//   function updateValue (e) {
+//     log.textContent = e.target.value
+//     console.log(e.target.value)
+//     answer = e.target.value
+//   }
+//   button.addEventListener('click', () => {
+//     console.log('respone: ' + result)
+//     postData(obj.nextURL, result)
+//   })
+// }
 
 async function getQuestion (id) {
   await fetch(`http://vhost3.lnu.se:20080/question/${id}`, { mode: 'cors' })
@@ -47,7 +50,7 @@ async function getQuestion (id) {
       function (res) {
         if (res.status !== 200) {
           console.log('Looks like there was a problem. Status Code: ' +
-          res.status)
+           res.status)
           return
         }
         // Examine the text in the response
@@ -62,6 +65,12 @@ async function getQuestion (id) {
       console.log('Fetch Error :-S', err)
     })
 }
+
+// input.addEventListener('change', e => {
+//   show.textContent = e.value
+//   body.appendChild(show)
+//   console.log(show)
+// })
 
 async function postData (url, data = {}) {
   await fetch(url, {
@@ -79,5 +88,20 @@ async function postData (url, data = {}) {
     getQuestion(res)
   })
 }
+
+const input = document.querySelector('#input')
+let answer = ''
+const button = document.querySelector('button')
+const questionSpan = document.querySelector('#question')
+
+function form (data) {
+  button.addEventListener('click', () => {
+    answer = input.value
+    postData()
+    console.log('TCL: answer', answer)
+    questionSpan.textContent = getQuestion(1).nextURL
+  })
+}
+
 getQuestion(1)
 // postData('http://vhost3.lnu.se:20080/answer/1', { answer: '2' })
